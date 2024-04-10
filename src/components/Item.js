686acd55b7a54214c40036ecbe25a3e39e770194
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2024-04-10 00:20:19 -0700

    complete lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isClicked, setIsClicked] = useState(false)
+
+  function handleClick() {
+    setIsClicked(isClicked => !isClicked)
+  }
+
+  const liClass = isClicked ? "in-cart" : "";
+  const btnCart = isClicked ? "Remove From Cart" : "Add to Cart";
+
   return (
-    <li className="">
+    <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}>{btnCart}</button>
     </li>
   );
 }
